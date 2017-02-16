b6d1438117d570a042bc39a4c2ebca21de590784
刘新宁 <liuxn@pagoda.com.cn>
2017-02-16 22:39:25 +0800

    Initial Commit

---
+++ CyGloberRouter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CyGloberRouter
+//
+//  Created by 刘新宁 on 2017/2/16.
+//  Copyright © 2017年 刘新宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CyGloberRouter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CyGloberRouter
+//
+//  Created by 刘新宁 on 2017/2/16.
+//  Copyright © 2017年 刘新宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
